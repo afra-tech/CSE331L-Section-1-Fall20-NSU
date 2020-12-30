16a00016273e695359b4511e2d69801b141569dd
Afra <afra.nawar05@northsouth.edu>
2020-12-30 19:19:45 +0600

    some parts more

---
+++ Quiz and Lab Assessment/Solution/1.asm
@@ -0,0 +1,33 @@
+                                                                                          
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+      
+MSG1    DB  13, 10, "Please Enter a String: $"   
+MSG2    DB  13, 10, "Palindrome of the String: $"     
+
+.CODE
+        
+ MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+    
+    LEA DX, MSG1
+    MOV AH, 9
+    INT 21H
+            
+    MOV AH, 2
+    INT 21H  
+   
+    MOV BL,AL 
+    
+  
+       
+    @EXIT:  
+    MOV AH, 4CH
+    INT 21H
+
+ 
+    MAIN ENDP
+    END MAIN
